bbb42d4dd55f63e41161caf22e56c0e61274bac3
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-12-08 07:08:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,15 +2,15 @@ import datetime
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
+    API_ID = environ.get("API_ID", "1923471")
+    API_HASH = environ.get("API_HASH", "fcdc178451cd234e63faefd38895c991")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Forward-Bot") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://mvideo:mvideo@cluster0.bpj54.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Forward")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '880087645').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002396553917'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002316896807") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
    
