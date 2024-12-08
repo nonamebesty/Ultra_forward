9dde52eef8de00d58e413364911ed90b17bc877d
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-12-08 10:25:06 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -84,7 +84,7 @@ async def pub_(bot, message):
             ):
                 if await is_cancelled(client, user, m, sts):
                    return
-                if pling %100 == 0: 
+                if pling %20 == 0: 
                    await edit(m, 'ᴘʀᴏɢʀᴇssɪɴɢ', 10, sts)
                 pling += 1
                 sts.add('fetched')
