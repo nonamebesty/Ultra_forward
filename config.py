e9af7d2a0e134a8fd3eb998d8527574cb21390e5
nonamebesty <160272703+nonamebesty@users.noreply.github.com>
2024-12-08 10:08:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Forward")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '880087645').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002396553917'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002316896807") 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "Egmore_links") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
    
